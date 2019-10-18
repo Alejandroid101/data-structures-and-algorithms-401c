acf12caa1daf9a956b3856987bde1e66a9baed3a
Alejandroid101 <kik.rdl3@gmail.com>
2019-10-18 11:34:13 -0700

    binarysearch method written, about to write test

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # data-structures-and-algorithms-401c
 
-# Challenge Summary
+# Challenge Summary for ArrayShift(CH1)
 I spent way too much time working on this. I ended up using a slightly different approach compared to what i did on the whiteboard
 
 ## Challenge Description
@@ -14,3 +14,16 @@ Then a for loop to iterate the new arraw and place the numbers in it
 Whiteboard pictures:
 https://github.com/Alejandroid101/data-structures-and-algorithms-401c/blob/master/challenges/ArrayShift/20191017_111700.jpg?raw=true
 https://github.com/Alejandroid101/data-structures-and-algorithms-401c/blob/master/challenges/ArrayShift/20191017_111656.jpg?raw=true
+
+
+# Challenge Summary for BinarySearch(CH2)
+
+## Challenge Description
+
+## Approach & Efficiency
+
+## Solution
+Whiteboard pictures:
+
+
+
---
+++ challenges/BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] one = { 1, 2, 3, 4, 5, 6, 7 };
+            //int[] two = { 8, 13, 27, 55, 57 };
+            //int[] three = { 6, 66, 666, 6666 };
+            //int[] four = { };
+            int one1 = 3;
+            //int two2 = 57;
+            //int three3 = 69;
+            //int four4 = 100;
+            Console.WriteLine(BinarySearch(one, one1));
+
+        }
+
+        public static int BinarySearch(int[] arr, int value)
+        {
+            int len = arr.Length;
+            int left = 0;
+            int right = len - 1;
+            while (left <= right)
+            {
+                int sum = left + right;
+                int mp = sum / 2;
+                if (arr[mp] < value)
+                {
+                    left = mp + 1;
+                }
+                else if(arr[mp] > value)
+                {
+                    right = mp - 1;
+                }
+                else
+                {
+                    return mp;
+                }
+            }
+                return -1;
+        }
+    }
+}
