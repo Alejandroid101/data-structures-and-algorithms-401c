9a220f81c9c5edcc8282e70d59bd1c8a34289dbb
Alejandroid101 <kik.rdl3@gmail.com>
2019-10-31 11:26:00 -0700

    Readme And image up. missing logic

---
+++ challenges/AnimalShelterForPetsWhoCannotReadGood/AnimalShelterForPetsWhoCannotReadGood/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+
+namespace AnimalShelterForPetsWhoCannotReadGood
+{
+    internal class Animal
+    {
+
+        public Animal(string animalName, string animalType)
+        {
+        AnimalName = animalName;
+        AnimalType = animalType;
+        }
+        public string AnimalName { get; set; }
+        public string AnimalType { get; set; }
+
+
+        Stack stack1 = new Stack();
+
+        Stack stack2 = new Stack();
+
+        public void Enqueue(string animal)
+        {
+            while (stack1.Count > 0)
+            {
+                stack2.Push(stack1.Pop());
+            }
+            stack1.Push(animal);
+        }
+
+        public string Dequeue(string pref)
+        {
+            string animal = (string)stack1.Pop();
+            return pref;
+        }
+    }
+}
---
+++ challenges/AnimalShelterForPetsWhoCannotReadGood/AnimalShelterForPetsWhoCannotReadGood/CatDog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalShelterForPetsWhoCannotReadGood
+{
+    class CatDog
+    {
+        public class PseudoQueue
+        {
+            //Stack stack1 = new Stack();
+
+            //Stack stack2 = new Stack();
+
+            //public void Enqueue(string animal)
+            //{
+            //    while (stack1.Count > 0)
+            //    {
+            //        stack2.Push(stack1.Pop());
+            //    }
+            //    stack1.Push(animal);
+            //}
+
+            //public string Dequeue(string pref)
+            //{
+            //    string animal = (string)stack1.Pop();
+            //    return pref;
+            //}
+
+        }
+    }
+}
