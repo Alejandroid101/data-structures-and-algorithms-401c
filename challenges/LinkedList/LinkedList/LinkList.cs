c55f49e17d5a0597d48fb172d43deac21e90ae24
Alejandroid101 <kik.rdl3@gmail.com>
2019-11-05 11:05:33 -0800

    read me is there.

--- challenges/LinkedList/LinkedList/LinkList.cs
+++ challenges/LinkedList/LinkedList/LinkList.cs
@@ -13,90 +13,90 @@ namespace LinkedList
             Head = null;
         }
 
-        public void Append(int value)
-        {
-            Node newNode = new Node(value);
-            if(Head == null)
-            {
-                Head = newNode;
-            }
-            else
-            {
-                Node current = Head;
-                while (current.Next != null)
-                {
-                    current = current.Next;
-                }
-                current.Next = newNode;
-            }
-        }
+        //public void Append(int value)
+        //{
+        //    Node newNode = new Node(value);
+        //    if(Head == null)
+        //    {
+        //        Head = newNode;
+        //    }
+        //    else
+        //    {
+        //        Node current = Head;
+        //        while (current.Next != null)
+        //        {
+        //            current = current.Next;
+        //        }
+        //        current.Next = newNode;
+        //    }
+        //}
 
 
-        public void insertBefore(int value, int newVal)
-        {
-            Node newNode = new Node(value);
-            if (Head == null)
-            {
-                Head = newNode;
-            }
-            else
-            {
+        //public void insertBefore(int value, int newVal)
+        //{
+        //    Node newNode = new Node(value);
+        //    if (Head == null)
+        //    {
+        //        Head = newNode;
+        //    }
+        //    else
+        //    {
 
-                Node current = Head;
-                while (current.Next != null)
-                {
-                    current = current.Next;
-                    try
-                    {
-                        if (current.Next.Data == newVal)
-                        {
-                            current.Next = newNode;
-                        }
-                    }
-                    catch(Exception e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
-                }
-                current.Next = newNode;
-            }
-        }
+        //        Node current = Head;
+        //        while (current.Next != null)
+        //        {
+        //            current = current.Next;
+        //            try
+        //            {
+        //                if (current.Next.Data == newVal)
+        //                {
+        //                    current.Next = newNode;
+        //                }
+        //            }
+        //            catch(Exception e)
+        //            {
+        //                Console.WriteLine(e.Message);
+        //            }
+        //        }
+        //        current.Next = newNode;
+        //    }
+        //}
 
 
-        public void insertAfter(int value, int newVal)
-        {
-            Node newNode = new Node(value);
-            if (Head == null)
-            {
-                Head = newNode;
-            }
-            else
-            {
-                Node current = Head;
-                while (current.Next != null)
-                {
-                    current = current.Next;
-                    try
-                    {
-                        if (current.Data == newVal)
-                        {
-                            current.Next = newNode;
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
-                }
-                current.Next = newNode;
-            }
-        }
+        //public void insertAfter(int value, int newVal)
+        //{
+        //    Node newNode = new Node(value);
+        //    if (Head == null)
+        //    {
+        //        Head = newNode;
+        //    }
+        //    else
+        //    {
+        //        Node current = Head;
+        //        while (current.Next != null)
+        //        {
+        //            current = current.Next;
+        //            try
+        //            {
+        //                if (current.Data == newVal)
+        //                {
+        //                    current.Next = newNode;
+        //                }
+        //            }
+        //            catch (Exception e)
+        //            {
+        //                Console.WriteLine(e.Message);
+        //            }
+        //        }
+        //        current.Next = newNode;
+        //    }
+        //}
         //CodeChallenge 5
         public void Insert(int value)
         {
             Node newNode = new Node(value);
             newNode.Next = Head;
-            HEad = newNode;
+            Head = newNode;
         }
 
         public bool Includes(int value)
@@ -119,7 +119,7 @@ namespace LinkedList
             string allValues = "";
             while(currentNode.Next != null)
             {
-                allvalues += $"{currentNode.Data}, ";
+                allValues += $"{currentNode.Data}, ";
                 currentNode = currentNode.Next;
             }
             allValues += currentNode.Data;
@@ -127,38 +127,38 @@ namespace LinkedList
 
         }
 
-        public int selectFromEnd(int k)
-        {
-            Node newNode = new Node();
-            if (Head == null)
-            {
-                Head = newNode;
-            }
-            else
-            {
-                int posK = 0;
-                int counter = 0;
-                Node current = Head;
-                while (current.Next != null)
-                {
-                    current = current.Next;
-                    counter++;
-                }
-                posK = counter - k;
-                counter = 0;
-                while (current.Next != null)
-                {
-                    if (counter == posK)
-                    {
-                        Console.WriteLine($"value of k found: {current.Data}");
-                        return current.Data;
-                    }
-                    current = current.Next;
-                    counter++;
-                }
-                return;
-            }
-        }
+        //public int SelectFromEnd(int k)
+        //{
+        //    Node newNode = new Node(k);
+        //    int val = 0;
+        //    if (Head == null)
+        //    {
+        //        Head = newNode;
+        //    }
+        //    else
+        //    {
+        //        int posK = 0;
+        //        int counter = 0;
+        //        Node current = Head;
+        //        while (current.Next != null)
+        //        {
+        //            current = current.Next;
+        //            counter++;
+        //        }
+        //        while (current.Next != null)
+        //        {
+        //            if (counter == posK)
+        //            {
+        //                Console.WriteLine($"value of k found: {current.Data}");
+        //                return current.Data;
+        //            }
+        //            current = current.Next;
+        //            counter++;
+        //            val = current.Data;
+        //        }
+        //    }
+        //        return val;
+        //}
 
     }
 }
