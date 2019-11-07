0dcebe54a0834ef4e4ff2761960c215b6ae2beb6
Alejandroid101 <kik.rdl3@gmail.com>
2019-11-07 00:04:23 -0800

    Tree logic from class 15 and Readme

---
+++ challenges/BinaryTreeCh15/BinaryTreeCh15/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryTreeCh15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Tree();
+        }
+
+        static void Tree()
+        {
+            Tree<int> tree = new Tree<int>(4);
+
+            Node<int> node2 = new Node<int>(8);
+            Node<int> node3 = new Node<int>(15);
+            Node<int> node4 = new Node<int>(16);
+            Node<int> node5 = new Node<int>(8);
+            Node<int> node6 = new Node<int>(8);
+            Node<int> node7 = new Node<int>(8);
+
+            tree.Root.LeftChild = node2;
+            tree.Root.RightChild = node3;
+            node2.LeftChild = node4;
+            node2.RightChild = node5;
+            node3.LeftChild = node6;
+            node3.RightChild = node7;
+
+
+            List<int> preOrder = tree.PreOrder(tree.Root);
+            Console.WriteLine(string.Join(',', preOrder));
+            Console.WriteLine();
+
+            List<int> inOrder = tree.InOrder(tree.Root);
+            Console.WriteLine(string.Join(',', inOrder));
+            Console.WriteLine();
+
+            List<int> postOrder = tree.PostOrder(tree.Root);
+            Console.WriteLine(string.Join(',', postOrder));
+            Console.WriteLine();
+
+            List<int> breadth = tree.BreadthFirst(tree.Root);
+            Console.WriteLine(string.Join(',', breadth));
+            Console.WriteLine();
+        }
+    }
+}
---
+++ challenges/BinaryTreeCh15/README.md
@@ -0,0 +1,18 @@
+# Challenge Summary for LinkedLists(CH5, CH6 and CH7)
+This challenge was about creating a console app that will traverse a tree in 4 dofferent ways:
+PostOrder, PreOrder, Inorder and Breadth First.
+
+-----
+## Challenge Description
+1.- Taking a Tree with values, This application will return the values in different order, depending
+on the aproach selected by the method utilized to traverse the tree. all 4 results will be different orders.
+
+------
+## Approach & Efficiency
+The 4 methods for challenge 15 were written in order.
+
+-------
+## Solution
+![Image 1](https://github.com/Alejandroid101/data-structures-and-algorithms-401c/blob/master/assets/CH15Tree.jpg?raw=true)
+
+
