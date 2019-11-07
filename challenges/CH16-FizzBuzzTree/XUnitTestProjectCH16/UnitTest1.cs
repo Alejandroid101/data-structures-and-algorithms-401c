bab9d176e767fa6f209846912965f7d09a3eba67
Alejandroid101 <kik.rdl3@gmail.com>
2019-11-07 13:51:41 -0800

    done with fizzbuzz, started ch10

---
+++ challenges/CH16-FizzBuzzTree/XUnitTestProjectCH16/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace XUnitTestProjectCH16
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+           
+        }
+    }
+}
